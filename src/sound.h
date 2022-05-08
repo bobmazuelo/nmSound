07cb0fde76af9d37cc2a4815e6e0369595b4768d
kodi2023 <miguelmazuelo92@gmail.com>
2022-05-08 10:52:10 +0200

    sound.h

--- src/sound.h
+++ src/sound.h
@@ -1,7 +1,11 @@
 #ifndef SOUND_H
 # define SOUND_H
 
-typedef struct	{};
+typedef struct s_note {
+	char	note;		// a b c d e f g
+	char	accident;	// +(sharp) -(flat)
+	int	duration;	//x1 x2 x4 x6 x8 x16 ...
+}	t_note;
 
 char	*read_file(int fd);
 #endif

a172f701589b3c9b2a481a02ecc0a69002eb8617
kodi2023 <miguelmazuelo92@gmail.com>
2022-05-19 17:14:50 +0200

    uodate

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-int	main()
+int	main(int argc, char **argv)
 {
 	int	mmfile;
        
-	mmfile = open("music.mm", O_RDONLY);
-	read_line(mmfile);
+	if (argc != 1)
+		raise("No file");
+	else
+	{
+		mmfile = open(argv[1], O_RDONLY);
+		read_file(mmfile);
+	}
 	return (0);
 }
--- src/read_file.c
+++ src/read_file.c
@@ -1,9 +1,9 @@
-# include <unistd.h>
-# include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include "sound.h"
 
-char	*read_line(int fd);
+char	*read_fine(int fd);
 
-statitc int	get_notes();
-statict int	get_duration();
+static int	get_notes();
+static int	get_duration();
